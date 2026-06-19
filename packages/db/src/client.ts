0ac0f199a4d0ac5f089690cd23eb4030537f6774
aaksant <aaksangit@gmail.com>
2026-06-19 21:37:32 +0700

    feat: add Drizzle and Supabase client

---
+++ packages/db/src/client.ts
@@ -0,0 +1,28 @@
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+import { createClient } from "@supabase/supabase-js";
+
+config({ path: "../../../.env" });
+
+const connectionString = process.env.DATABASE_URL!;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined.");
+}
+
+const client = postgres(connectionString);
+const supabaseUrl = process.env.SUAPABASE_URL!;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+
+if (!supabaseUrl || !supabaseServiceRoleKey) {
+  throw new Error("Missing Supabase credentials.");
+}
+
+export const db = drizzle(client, {
+  schema,
+  logger: true
+});
+
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
